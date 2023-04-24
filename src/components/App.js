9f99a07b0295cbf8767681c49d1cc3975bd5841c
arjundevlop <arjun.tomar45@gmail.com>
2023-04-24 15:38:54 +0530

    tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
+
+const tabs = [{Tab1:"This is the content for Tab1"},
+{Tab2:"This is the content for Tab2"},
+{Tab3:"This is the content for Tab3"}];
 
 const App = () => {
+  const [value, setvalue] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ul>
+          <li onClick={()=>{setvalue(tabs[0].Tab1)}}>Tab 1</li>
+          <li onClick={()=>{setvalue(tabs[1].Tab2)}}>Tab 2</li>
+          <li onClick={()=>{setvalue(tabs[2].Tab3)}}>Tab 3</li>
+        </ul>
+        <div>{value}</div>
     </div>
   )
 }
